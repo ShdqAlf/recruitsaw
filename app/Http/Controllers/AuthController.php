f667dabbfb88bbc2e813f2486ea612aba44e1474
ShdqAlf <shdqalf01@gmail.com>
2024-09-18 15:09:31 +0700

    update

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+
+class AuthController extends Controller
+{
+    public function showLoginForm()
+    {
+        return view('auth.login');
+    }
+
+    public function login(Request $request)
+    {
+        $credentials = $request->only('email', 'password');
+
+        if (Auth::attempt($credentials)) {
+            // Jika login berhasil
+            return redirect()->intended('dashboard');
+        }
+
+        // Jika login gagal
+        return redirect()->back()->withErrors(['email' => 'Email atau password salah.']);
+    }
+
+    public function logout()
+    {
+        Auth::logout();
+        return redirect('/');
+    }
+}
